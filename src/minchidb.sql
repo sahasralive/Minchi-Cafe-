982dde1a51a0d8543a5f95a845fc70c732dccbc6
Sahasra De Silva <111166211+sahasralive@users.noreply.github.com>
2023-04-06 21:13:37 +0530

    Add files via upload

--- src/minchidb.sql
+++ src/minchidb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 05, 2023 at 05:16 PM
+-- Generation Time: Apr 06, 2023 at 05:42 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -34,6 +34,20 @@ CREATE TABLE `Bill` (
   `Amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `Bill`
+--
+
+INSERT INTO `Bill` (`Bill No`, `Date`, `Seller`, `Amount`) VALUES
+(1, '2023-04-06', 'User1', 3070),
+(2, '2023-04-06', 'User1', 10000),
+(3, '2023-04-06', 'User1', 100),
+(4, '2023-04-06', 'User1', 820),
+(5, '2023-04-06', 'User1', 1440),
+(6, '2023-04-06', 'User1', 1250),
+(7, '2023-04-06', 'User1', 460),
+(8, '2023-04-06', 'User1', 450);
+
 -- --------------------------------------------------------
 
 --
@@ -55,7 +69,30 @@ INSERT INTO `Product` (`ID`, `PName`, `Category`, `Price`) VALUES
 (2, 'Chips', 'Snacks', 190),
 (3, 'Burger', 'Food Items', 1250),
 (4, 'Coca cola', 'Beverages', 270),
-(5, 'Lime Juice', 'Juice', 190);
+(5, 'Lime Juice', 'Juice', 190),
+(6, 'mojito lime', 'Snacks', 260),
+(8, 'biscuit', 'Snacks', 100),
+(9, 'mojito mango', 'Snacks', 360);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `UserID` int(4) NOT NULL,
+  `Username` varchar(40) NOT NULL,
+  `Password` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `User`
+--
+
+INSERT INTO `User` (`UserID`, `Username`, `Password`) VALUES
+(1, 'Admin', 'Password'),
+(2, 'user', '123');
 
 --
 -- Indexes for dumped tables
@@ -74,6 +111,12 @@ ALTER TABLE `Product`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`UserID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -81,13 +124,19 @@ ALTER TABLE `Product`
 -- AUTO_INCREMENT for table `Bill`
 --
 ALTER TABLE `Bill`
-  MODIFY `Bill No` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `Bill No` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `Product`
 --
 ALTER TABLE `Product`
-  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `UserID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
